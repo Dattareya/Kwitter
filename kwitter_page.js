b9ff7024b369799eb11468a753c486c9e2d62e8e
Dattareya <82696837+Dattareya@users.noreply.github.com>
2022-06-29 16:16:31 +0530

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -22,16 +22,30 @@ function send()
  document.getElementById("msg").value="";
 }
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
-       firebase_message_id = childKey;
-       message_data = childData;
+         firebase_message_id = childKey;
+         message_data = childData;
+         console.log(message_data);
+
 //Start code
-          name=message_data['name'];
-          message=message_data['message'];
-          like=message_data['like'];
-          nametag="<h4>"+name+"</h4>";
-          messagetag="<h4 class='message_h4'> "+message+"</h4>";
-          likebutton="<button class='btn btn-info ' id="+firebase_message_id+" value="+like + " onclick='updatelike(this.like)'>";
-          
+            name=message_data['name'];
+            message=message_data['message'];
+            like=message_data['like'];
+            nametag="<h4>"+name+"</h4>";
+            messagetag="<h4 class='message_h4'> "+message+"</h4>";
+            likebutton="<button class='btn btn-info ' id="+firebase_message_id+" value="+like + " onclick='updatelike(this.like)'>";
+            spanintag="<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
+            row=nametag+messagetag+likebutton+spanintag;
+            document.getElementById("output").innerHTML += row;
+
 //End code
-    } });  }); }
+      } });  }); }
 getData();
+function updateLike(message_id)
+ { console.log("clicked on like button - " + message_id);
+  button_id = message_id; likes = document.getElementById(button_id).value; updated_likes = Number(likes) + 1;
+   console.log(updated_likes); 
+   firebase.database().ref(room_name).child(message_id).update({ like : updated_likes });
+   } 
+function logout() { localStorage.removeItem("user_name"); 
+localStorage.removeItem("room_name"); 
+window.location.replace("kwitter.html"); }
